f71d36412cb4ca8d4b6f50cb21fa9ee11c6cdee4
Timothy <timothy.mead20@gmail.com>
2022-03-03 16:33:11 +1000

    Fixed project include

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "modules/modules.h"
+#include "Modules/modules.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
